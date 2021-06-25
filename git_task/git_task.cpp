b539cc9827906f4c0d65691e6ed2524021ad5639
FigureScatingLover <lodochkina2001victoria@gmail.com>
2021-06-25 09:33:29 +0300

    answer with comments

--- git_task/git_task.cpp
+++ git_task/git_task.cpp
@@ -7,6 +7,22 @@
 using namespace std;
 
 int main() {
-	
+	string str; 
+	cin >> str;
+	int len = str.length();
+	for (int i = len - 1; i > 0; i--) { 
+		if (str[i - 1] < str[i]) {
+			int p = i;
+			for (int j = p; j < len; j++) {
+				if (str[j] <= str[p] && str[i - 1] < str[j]) p = j; 
+			}
+			swap(str[i - 1], str[p]); 
+			for (int j = len - 1; i < j; i++, j--) { 
+				swap(str[i], str[j]);
+			}
+			break;
+		}
+	}
+	cout << str;
 	return 0;
 }
