6da7c37ccc923f26a2607b9168d46410c83d74e9
FigureScatingLover <lodochkina2001victoria@gmail.com>
2021-06-25 09:43:47 +0300

    the end

--- git_task/git_task.cpp
+++ git_task/git_task.cpp
@@ -3,21 +3,27 @@
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-	string str; 
-	cin >> str;
+	string str;
+
+	ifstream fromfile("file.txt");
+	fromfile >> str;
+ 
+	//cin >> str;
 	int len = str.length();
-	for (int i = len - 1; i > 0; i--) { 
+	//алгоритм Нарайяны (поиск следующей лексикографической последовательности)
+	for (int i = len - 1; i > 0; i--) { //поиск наибольшего индекса
 		if (str[i - 1] < str[i]) {
 			int p = i;
 			for (int j = p; j < len; j++) {
-				if (str[j] <= str[p] && str[i - 1] < str[j]) p = j; 
+				if (str[j] <= str[p] && str[i - 1] < str[j]) p = j; //поворот
 			}
-			swap(str[i - 1], str[p]); 
-			for (int j = len - 1; i < j; i++, j--) { 
+			swap(str[i - 1], str[p]); //суфф<->поворот
+			for (int j = len - 1; i < j; i++, j--) { //разворот суффикса
 				swap(str[i], str[j]);
 			}
 			break;
