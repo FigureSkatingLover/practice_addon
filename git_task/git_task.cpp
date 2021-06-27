820c76d2bf69e0784a3befc465c2793e64679c3c
FigureScatingLover <lodochkina2001victoria@gmail.com>
2021-06-28 00:02:48 +0300

    with .h

--- git_task/git_task.cpp
+++ git_task/git_task.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#include "git_task.h"
+#include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -6,24 +7,6 @@
 #include <fstream>
 
 using namespace std;
-string anagram(string str) {
-	int len = str.length();
-	//алгоритм Нарайяны (поиск следующей лексикографической последовательности)
-	for (int i = len - 1; i > 0; i--) { //поиск наибольшего индекса
-		if (str[i - 1] < str[i]) {
-			int p = i;
-			for (int j = p; j < len; j++) {
-				if (str[j] <= str[p] && str[i - 1] < str[j]) p = j; //поворот
-			}
-			swap(str[i - 1], str[p]); //суфф<->поворот
-			for (int j = len - 1; i < j; i++, j--) { //разворот суффикса
-				swap(str[i], str[j]);
-			}
-			break;
-		}
-	}
-	return str;
-}
 
 int main() {
 	string str;
---
+++ git_task/git_task.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+
+using namespace std;
+string anagram(string str) {
+	int len = str.length();
+	//алгоритм Нарайяны (поиск следующей лексикографической последовательности)
+	for (int i = len - 1; i > 0; i--) { //поиск наибольшего индекса
+		if (str[i - 1] < str[i]) {
+			int p = i;
+			for (int j = p; j < len; j++) {
+				if (str[j] <= str[p] && str[i - 1] < str[j]) p = j; //поворот
+			}
+			swap(str[i - 1], str[p]); //суфф<->поворот
+			for (int j = len - 1; i < j; i++, j--) { //разворот суффикса
+				swap(str[i], str[j]);
+			}
+			break;
+		}
+	}
+	return str;
+}
